1ff744de07b7f052a52b405bef4b875ac1ec6aba
okota <krknkiki@gmail.com>
2025-10-21 22:56:27 +0900

    地図を画面内に収めた

--- src/components/PrefectureMap.tsx
+++ src/components/PrefectureMap.tsx
@@ -58,5 +58,7 @@ export const PrefectureMap = () => {
     loadMap()
   }, [])
 
-  return <div ref={containerRef} className="w-full max-w-4xl mx-auto" />
+  return (
+    <div ref={containerRef} className="w-full max-w-2xl mx-auto p-4 md:p-8" />
+  )
 }
